5b8098daf76f5730ded6db6fb089a47a30574d73
Jacob Poor <jacob.poor98@gmail.com>
2022-03-02 09:51:04 -0700

    Initial

---
+++ Mission07/Models/Basket.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mission07.Models
+{
+    public class Basket
+    {
+        public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
+
+        public void AddItem (Book book, int qty)
+        {
+            BasketLineItem line = Items
+                .Where(b => b.Book.BookId == book.BookId)
+                .FirstOrDefault();
+
+            // this wil either start a new basket if no basket yet exists
+            if (line == null)
+            {
+                Items.Add(new BasketLineItem
+                {
+                    Book = book,
+                    Quantity = qty
+                });
+            }
+            // else it will add onto the existing basket
+            else
+            {
+                line.Quantity += qty;
+            }
+        }
+
+        // create a calculate total function
+        public double CalculateTotal()
+        {
+            double sum = Items.Sum(x => x.Quantity * x.Book.Price);
+
+            return sum;
+        }
+    }
+
+    // create the basket class
+    public class BasketLineItem
+    {
+        public int LineID { get; set; }
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Mission07/Pages/Purchase.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission07.Infrastructure;
+using Mission07.Models;
+
+namespace Mission07.Pages
+{
+    public class PurchaseModel : PageModel
+    {
+        // bring in the IBookProjectRepository and assign it to a variable to be
+        // used as an instance in this class
+        private IBookProjectRepository repo { get; set; }
+
+        public PurchaseModel (IBookProjectRepository temp)
+        {
+            repo = temp;
+        }
+
+        public Basket basket { get; set; }
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUrl)
+        {
+            // take them back to the returnUrl if exists, else take them to the homepage
+            ReturnUrl = returnUrl ?? "/";
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+        }
+
+        public IActionResult OnPost(int bookId, string returnUrl)
+        {
+            // add the book to the cart via the book Id
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+            basket.AddItem(b, 1);
+
+            HttpContext.Session.SetJson("basket", basket);
+
+            // redirect back to the ReturnUrl
+            return RedirectToPage(new { ReturnUrl = returnUrl });
+        }
+    }
+}
